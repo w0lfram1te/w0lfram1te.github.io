162fb047139dcd86f80b7a32e6ab1af2e4aa7261
w0lfram1te <74439854+w0lfram1te@users.noreply.github.com>
2026-01-11 15:40:17 +0800

    configure tailwind to reference classes instead of strictly user preference; added js to control the theme via button onclick events

---
+++ assets/js/theme.js
@@ -0,0 +1,40 @@
+applyTheme();
+
+// create elements with the indicated #ids to manipulate the theme
+window.onload = (event) => {
+	const darkModeToggle = document.querySelector('#darkModeToggle');
+	darkModeToggle.addEventListener('click', toggleDarkMode);
+
+	const setLight = document.querySelector('#setlightMode');
+	const setDark = document.querySelector('#setdarkMode');
+	setLight.addEventListener('click', setLightMode);
+	setDark.addEventListener('click', setDarkMode);
+};
+
+function applyTheme() {
+  document.body.classList.toggle(
+    "dark",
+    localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+  );
+};
+
+function setLightMode() {
+	localStorage.theme = "light";
+	document.body.classList.remove("dark");
+}
+
+function setDarkMode() {
+	localStorage.theme = "dark";
+	document.body.classList.add("dark");
+}
+
+function toggleDarkMode() {
+	document.body.classList.toggle("dark");
+
+	if (document.body.classList.contains("dark")) {
+		localStorage.theme = "dark"
+	} else {
+		localStorage.theme = "light"
+	}
+}
